61d5dbb84ebe0f104252f682aa1c62507947908c
Khaled_Hamdy <99548164+Khaled-Hamdy1@users.noreply.github.com>
2025-04-26 16:43:35 +0300

    feat: add server schema, setup page, and initial modal
    Introduce server schema for validation, create setup page to handle server initialization, and add initial modal for server customization. Also, remove debug logs from middleware and update dependencies for better compatibility and performance.

---
+++ src/schema/server.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const serverSchema = z.object({
+  name: z.string().min(1, { message: 'Server name is required' }),
+  imageUrl: z.string().min(1, { message: 'Server image URL is required' }),
+})
+
+export type Server = z.infer<typeof serverSchema>
